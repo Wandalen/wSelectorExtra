40e56ff375c39ef377dd1c0dbb56a5c431733b25
volodymyrmigdal <volodymyr.migdal@gmail.com>
2020-04-01 13:41:31 +0300

    update jsdoc

--- proto/dwtools/abase/l6/SelectorExtra.s
+++ proto/dwtools/abase/l6/SelectorExtra.s
@@ -5,6 +5,7 @@
 /**
  * Collection of routines to select a sub-structure from a complex data structure. The module extends module Selector.
   @module Tools/base/SelectorExtra
+  @extends Tools
 */
 
 /**
@@ -12,9 +13,7 @@
  */
 
 /**
- *@summary Collection of routines to select a sub-structure from a complex data structure.
-  @namespace Tools.SelectorExtra 
-  @memberof module:Tools/base/SelectorExtra
+  Collection of routines to select a sub-structure from a complex data structure.
 */
 
 if( typeof module !== 'undefined' )
@@ -84,7 +83,7 @@ _entityProbeReport.defaults =
  * @param {Boolean} o.report=1 Supplements result with info about sub-structures.
  * @param {String} o.title Title of report string. By default uses value of `o.selector`.
  * @function entityProbeField
- * @memberof module:Tools/base/SelectorExtra.Tools( module::SelectorExtra )
+ * @memberofs "module:Tools/base/SelectorExtra","module:Tools/base/Fundamental.wTools"
 */
 
 function entityProbeField( o )
@@ -178,7 +177,7 @@ entityProbeField.defaults.report = 1;
  * //*.d : 1 [ Array with 1 elements ]
  *
  * @function entityProbe
- * @memberof module:Tools/base/SelectorExtra.Tools( module::SelectorExtra )
+ * @memberofs "module:Tools/base/SelectorExtra","module:Tools/base/Fundamental.wTools"
 */
 
 function entityProbe( o )
