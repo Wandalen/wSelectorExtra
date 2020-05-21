20ef8b3d1ce770fba438053f55ef03bbf2776278
wandalen <wandalen.me@gmail.com>
2020-05-21 17:48:06 +0300

    .

--- proto/dwtools/abase/l6/SelectorExtra.s
+++ proto/dwtools/abase/l6/SelectorExtra.s
@@ -314,7 +314,7 @@ _.mapSupplement( Self, Proto );
 // export
 // --
 
-if( typeof module !== 'undefined' && module !== null )
+if( typeof module !== 'undefined' )
 module[ 'exports' ] = Self;
 
 })();
