fd14fc9729ebf2c998ed54aff4dea52eca1cfe27
wandalen <wandalen.me@gmail.com>
2020-07-16 16:31:54 +0300

    .

--- proto/wtools/abase/l6.test/SelectorExtra.test.s
+++ proto/wtools/abase/l6.test/SelectorExtra.test.s
@@ -14,8 +14,8 @@ if( typeof module !== 'undefined' )
 
 }
 
-var _global = _global_;
-var _ = _global_.wTools;
+let _global = _global_;
+let _ = _global_.wTools;
 
 // --
 // tests
@@ -40,7 +40,7 @@ function entityProbe( test )
 // declare
 // --
 
-var Self =
+let Self =
 {
 
   name : 'Tools.base.l6.SelectorExtra',
